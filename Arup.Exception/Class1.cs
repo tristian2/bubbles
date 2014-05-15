7d17624d09f5c9dda8bde2c7fbf2a2b2a6958cfc
tristian o'brien <tristian2@hotmail.com>
2014-05-15 09:44:17 +0100

    first push with mistakes

---
+++ Arup.Exception/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arup.Exception
+{
+    public class Custom :System.Exception
+    {
+        public Custom() { }
+
+        public Custom(String message) : base(message) { }
+
+        public Custom(String message, System.Exception innerException) : base(message, innerException) { }
+
+    }
+}
