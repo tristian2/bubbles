336f92c9464765bcd8c287e699ce745d6f9fcfef
tristian o'brien <tristian2@hotmail.com>
2014-05-15 09:46:34 +0100

    first attempt 2

---
+++ Arup.Math/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arup.Exception;
+
+namespace Arup.Math
+{
+    public class Divide
+    {
+
+        public static Double divide(Double number)
+        {
+            Double result = 0;
+            try
+            {
+                result = number / 0;
+                throw new DivideByZeroException ();
+            }
+            catch (Arup.Exception.Custom arupex)
+            {
+                throw new Exception.Custom("custom arup error" + arupex.Message, arupex);                
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+    }
+}
---
+++ RunBuubles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunBuubles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("bubbling errors...");
+
+            Console.WriteLine("a division by zero error");
+
+            try {
+                double result = Arup.Math.Divide.divide(123);
+                Console.WriteLine(result.ToString());
+            }
+            catch(Exception ex) {
+                Console.WriteLine("error occured" + ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
